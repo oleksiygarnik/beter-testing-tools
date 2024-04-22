fb88652ea80978f20ba7ca99ae10ffad3a2d148e
o.harnyk <o.harnyk@beter.co>
2024-04-22 15:41:48 +0300

    Init project

---
+++ tests/Beter.TestingTools.Emulator.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Beter.TestingTools.Emulator.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Beter.TestingTools.Generator.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Beter.TestingTools.Generator.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
